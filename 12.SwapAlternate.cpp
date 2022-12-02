3c3fcfa1822dc48c638e9a8f72246719f98d6c83
Mehak <77800400+Mehak0111@users.noreply.github.com>
2022-12-02 19:35:47 +0530

    Add files via upload

---
+++ 12.SwapAlternate.cpp
@@ -0,0 +1,20 @@
+//swap alternate 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=1;i<=n;i++){
+		cin>>arr[i];
+	}
+	for(int i=1;i<=n;i+=2){
+		if(i+1<n){
+		swap(arr[i],arr[i+1]);
+}
+	}
+	for(int i=1;i<=n;i++){
+		cout<<arr[i]<<" ";
+	}
+}
